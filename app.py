08136c9c7d77a0eaf5bb0504b731fc9ca856ec04
Mike Hardy <michaeldavidhardy@gmail.com>
2016-02-09 10:27:42 -0800

    first commit

--- app.py
+++ app.py
@@ -10,50 +10,33 @@ app = Flask(__name__)
 app.vars={}
 
 
-template = jinja2.Template("""
-<!DOCTYPE html>
-<html lang="en-US">
-<link rel="stylesheet" href="https://cdn.pydata.org/bokeh/release/bokeh-0.11.0.min.css" type="text/css"/>
-<script type="text/javascript" src="https://cdn.pydata.org/bokeh/release/bokeh-0.11.0.min.js"></script>
-<body>
-    {{ script }}
-    {{ div }}
-    <form id='userinfoform_tckr' method='get' action='index_tckr' >
-    <p>
-    <input type='submit' value='Go Back' />
-    </p>
-    </form>
-</body>
-</html>
-""")
+@app.route('/')
+def index_tckr():  
+	return render_template('userinfo_tckr.html')
 
 
-@app.route('/index_tckr', methods=['GET','POST'])
-def index_tckr():
-	if request.method == 'GET':   
-		return render_template('userinfo_tckr.html')
-	else: 
-		app.vars['Ticker'] = request.form['name_tckr']
-		stock_data = pd.read_csv("https://www.quandl.com/api/v3/datasets/WIKI/%s.csv?auth_token=SPdj1tC3fV-eeCdt9YGq" % app.vars['Ticker'],
-        parse_dates=['Date'] )
-        
-    	p = figure(width=800, height=250, x_axis_type="datetime",\
-    		title="%s" %app.vars['Ticker'].upper(), title_text_font_size='10pt')
-    	p.line(stock_data['Date'], stock_data['Close'], color='navy', alpha=0.5)
-    	p.yaxis.axis_label = "End of Day (USD)"
-    	p.yaxis.axis_label_text_font_size = "9pt"
-    	    	
-    	script, div = components(p)
-    	show(p)
-    	print type(p)
-    	HTML(template.render(script=script, div=div))
-    	return template.render(script=script, div=div)
+@app.route('/graph', methods=['POST'])
+def generate_graph():
+	app.vars['Ticker'] = request.form['name_tckr']
+	stock_data = pd.read_csv("https://www.quandl.com/api/v3/datasets/WIKI/%s.csv?auth_token=SPdj1tC3fV-eeCdt9YGq" \
+			% app.vars['Ticker'], parse_dates=['Date'] )
+			
+	p = figure(width=800, height=250, x_axis_type="datetime",title="%s" \
+			% app.vars['Ticker'].upper(), title_text_font_size='10pt')
+	p.line(stock_data['Date'], stock_data['Close'], color='navy', alpha=0.5)
+	p.yaxis.axis_label = "End of Day (USD)"
+	p.yaxis.axis_label_text_font_size = "9pt"
+	
+	script, div = components(p)
+	show(p)
+	return render_template('graph.html', script=script, div=div)
 
 
 @app.errorhandler(500)
 def pageNotFound(error):
 	return render_template('error_page.html')
-    
+
+  
 @app.errorhandler(404)
 def pageNotFound(error):
 	return render_template('error_page.html')
