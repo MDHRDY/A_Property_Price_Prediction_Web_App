d07b24086d54d9eb93a3e5a037af1329a1e6d6dd
Mike Hardy <michaeldavidhardy@gmail.com>
2016-04-17 23:32:47 -0700

    initial commit

--- app.py
+++ app.py
@@ -2,33 +2,32 @@ from flask import Flask, render_template, request, redirect
 import pandas as pd
 from bokeh.embed import components 
 from bokeh.plotting import figure, output_file, show
+from bokeh.io import output_notebook, show
+from bokeh.charts import Scatter
 import jinja2
 from IPython.display import HTML
+import numpy as np
+import matplotlib.pyplot as plt
 
-app = Flask(__name__)
+
+
+
+app = Flask(__name__, static_url_path = "", static_folder = "")
 
 app.vars={}
 
 
-@app.route('/')
-def index_tckr():  
-	return render_template('userinfo_tckr.html')
-
-
-@app.route('/graph', methods=['POST'])
+@app.route('/graph', methods=['GET','POST'])
 def generate_graph():
-	app.vars['Ticker'] = request.form['name_tckr']
-	stock_data = pd.read_csv("https://www.quandl.com/api/v3/datasets/WIKI/%s.csv?auth_token=SPdj1tC3fV-eeCdt9YGq" \
-			% app.vars['Ticker'], parse_dates=['Date'] )
-			
-	p = figure(width=800, height=250, x_axis_type="datetime",title="%s" \
-			% app.vars['Ticker'].upper(), title_text_font_size='10pt')
-	p.line(stock_data['Date'], stock_data['Close'], color='navy', alpha=0.5)
-	p.yaxis.axis_label = "End of Day (USD)"
-	p.yaxis.axis_label_text_font_size = "9pt"
-	
-	script, div = components(p)
-	show(p)
+	df = pd.read_csv('For_Regression.csv')
+	xval = df['Latitude'].tolist()
+	yval = df['Longitude'].tolist()
+	plot = figure(width=500, height=500,title='Brooklyn Property Listings')
+	plot.yaxis.axis_label = "Latitude"
+	plot.xaxis.axis_label = "Longitude"
+
+	plot.scatter(yval,xval)
+	script, div = components(plot)
 	return render_template('graph.html', script=script, div=div)
 
 
@@ -43,5 +42,5 @@ def pageNotFound(error):
 
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True)
     
